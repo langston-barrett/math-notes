c1bea3bf7fbe93ab213b6dc4b8d414b2cada3a0e
Langston Barrett <langston.barrett@gmail.com>
2018-03-28 23:18:48 +0000

    crypto: init

--- default.nix
+++ default.nix
@@ -1,6 +1,27 @@
 { pkgs ? import <nixpkgs> { } }:
 
-with pkgs; stdenv.mkDerivation rec {
+# let
+  # amsthm_to_anki = with pkgs; stdenv.mkDerivation {
+  #   name = "amsthm-to-anki"
+  #   version = "0.1.0";
+  #   src = fetchFromGitHub {
+  #     owner = "siddharthist";
+  #     repo = "amsthm-to-anki";
+  #     rev = "80a65d70a69c802eef9a86855c406ea1ed575427";
+  #     sha256 = "1mg5fwfm2690ci7yhx7b84gqm2gynkfm8s13ipglsnbzqvxqd54p";
+  #   }
+
+  #   buildInputs = [
+  #     gcc
+  #     git
+  #     gmp
+  #     haskellPackages.stack
+  #   ];
+
+  #   buildPhase =
+  # }
+# in
+with pkgs; stdenv.mkDerivation {
   name = "math-notes";
   version = "0.1.0";
 
@@ -9,16 +30,18 @@ with pkgs; stdenv.mkDerivation rec {
   phases = [ "unpackPhase" "buildPhase" "installPhase" ];
 
   buildPhase = ''
-    for texfile in *.tex; do
+    for texfile in crypto.tex group-theory.tex; do
       latexmk -lualatex "$texfile"
     done
   '';
 
   installPhase = ''
+    mkdir -p "$out"
     cp *.pdf $out
   '';
 
   buildInputs = [
+    # amsthm_to_anki
     noto-fonts
     texlive.combined.scheme-full # lualatex, etc.
   ];
